1e6876cb9626602c5e42c113eda1519fde26b7b5
elledienne <elledienne@gmail.com>
2015-10-21 14:11:10 -0700

    gonna sawp to new branch

--- client/views/PlayerView.js
+++ client/views/PlayerView.js
@@ -1,9 +1,20 @@
 // PlayerView.js - Defines a backbone view class for the music player.
 var PlayerView = Backbone.View.extend({
+  events:{
+    // "click"
+
+
+  },
 
   // HTML5 (native) audio tag is being used
   // see: https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Using_HTML5_audio_and_video
-  el: '<audio controls autoplay />',
+  // el: '<audio id="the_player" controls autoplay />',
+  el: '<audio id="the_player" controls autoplay /><div>\
+      <button onclick="document.getElementById("the_player").play()">Play the Audio</button>\
+      <button onclick="document.getElementById("the_player").pause()">Pause the Audio</button>\
+      <button onclick="document.getElementById("the_player").volume+=0.1">Increase Volume</button>\
+      <button onclick="document.getElementById("the_player").volume-=0.1">Decrease Volume</button>\
+      </div>',
 
   initialize: function() {
   },
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,12 +1,37 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
+  tagName: "table",
 
   initialize: function() {
+    // this.on("add",this.render,this) //might not be what I want? - might want to do thi in a entryview
+
+    this.render();
+
   },
 
   render: function() {
-  initialize: function() {
-  },
+    // to preserve event handlers on child nodes, we must call .detach() on them before overwriting with .html()
+    // see http://api.jquery.com/detach/
+    this.$el.children().detach();
+
+    this.$el.html('<th>The Song Queue</th>').append(
+      this.collection.map(function(song) {
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
   }
 
+
+
+
+
+
+
+
+  // render: function() {
+  // initialize: function() {
+  // },
+  // }
+
 });
+
